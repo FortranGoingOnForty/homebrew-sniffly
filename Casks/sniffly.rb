59cd8e3bf473775f8e58259ef45d3657a9cd1e9d
espadonne <espadonne@outlook.com>
2025-11-13 16:44:32 -0500

    bump to 0.4.3

--- Casks/sniffly.rb
+++ Casks/sniffly.rb
@@ -1,6 +1,6 @@
 cask "sniffly" do
-  version "0.4.2"
-  sha256 "930c6e7ecec88e309b273c4f0e17e15725a86d94a5dd22b204db14e45543f25f"
+  version "0.4.3"
+  sha256 "52c8a18288c15460a9612a9c08324541471ebf2ccec3879970c6b3e7afdf23b8"
 
   url "https://github.com/FortranGoingOnForty/sniffly/releases/download/v#{version}/Sniffly-#{version}-macOS.zip"
   name "Sniffly"
